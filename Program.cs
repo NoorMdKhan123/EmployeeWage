e0ac05a59085dde9455a5d62c16ffe4ac5993d38
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-21 00:05:16 +0530

    Checking employee attendance

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace EmployeeWageProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            int employee_is_present = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == employee_is_present)
+                Console.WriteLine("Employee is Present");
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
         }
     }
 }
